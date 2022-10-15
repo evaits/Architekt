b2278eedca144395f44ecd9bef32778a3cd2487e
evaits <musicevait@gmail.com>
2022-10-16 00:07:27 +0200

    do all, start fixed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Tabs projects
 const tabsBtn = document.querySelectorAll(".projects-list-item")
 const tabsItems = document.querySelectorAll(".projects-content")
 
@@ -24,3 +25,30 @@ tabsBtn.forEach(function(item) {
 
 document.querySelector('.projects-list-item').click();
 
+
+// Tabs persons
+const ptabsBtn = document.querySelectorAll(".persons-radio")
+const ptabsItems = document.querySelectorAll(".persons-element")
+
+ptabsBtn.forEach(function(items) {
+    items.addEventListener("click", function() {
+        let currentBtnp = items;
+        let tabIdp = currentBtnp.getAttribute('data-tab');
+        let currentTabp = document.querySelector(tabIdp);
+        
+
+        ptabsBtn.forEach(function(items){
+            items.classList.remove('persons-radio-active');
+        });
+
+        ptabsItems.forEach(function(items){
+            items.classList.remove('persons-element-active');
+        });
+
+        currentBtnp.classList.add('persons-radio-active');
+
+        currentTabp.classList.add('persons-element-active');
+    });
+});
+
+document.querySelector('.persons-radio').click();
